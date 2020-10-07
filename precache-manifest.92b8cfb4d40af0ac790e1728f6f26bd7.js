c6746149240bb0af7f99a9a56f8f8178a2f61e90
misterblack0 <misterblack0@protonmail.com>
2020-10-07 18:39:12 +0300

    Updates

---
+++ precache-manifest.92b8cfb4d40af0ac790e1728f6f26bd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af9280bc292e0c47e761c28ba5f6899",
+    "url": "/notes-app-react/index.html"
+  },
+  {
+    "revision": "9f602c02fc2d90253197",
+    "url": "/notes-app-react/static/js/2.0a0e73b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/notes-app-react/static/js/2.0a0e73b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6316e38cd0f3c17db70",
+    "url": "/notes-app-react/static/js/main.b0403083.chunk.js"
+  },
+  {
+    "revision": "605619d3f091cf426153",
+    "url": "/notes-app-react/static/js/runtime-main.403c6972.js"
+  }
+]);
